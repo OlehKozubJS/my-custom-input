4bc3cb14c48117b6b197876e5f7123cbaf202e31
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-10 17:55:32 +0200

    Update MyInput.jsx

--- src/components/MyInput/MyInput.jsx
+++ src/components/MyInput/MyInput.jsx
@@ -1,15 +1,15 @@
-import { Component } from "react";
-import css from './MyInput.module.css';
+import { Component } from 'react';
+import {} from './MyInput.module.css';
 
 export class MyInput extends Component {
-    render() {
-        const { children } = this.props;
+  render() {
+    const { children } = this.props;
 
-        return (
-            <div className={css.inputContainer}>
-                <div className={css.inputContent}>{children}</div>
-                <button className={css.enterButton}>E</button>
-            </div>
-        )
-    }
-} 
+    return (
+      <div className={}>
+        <div className={}>{children}</div>
+        <button className={}>E</button>
+      </div>
+    );
+  }
+}
