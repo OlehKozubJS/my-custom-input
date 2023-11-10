5fb5592fe5da024857b096c2b83528c45fe48b8d
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-10 22:06:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
 //import ReactDOM from 'react-dom/client';
 //import './index.css';
 const { StrictMode } = require('react');
-const {} = require('react/client');
+const { createRoot } = require('react-dom/client');
 const { App } = require('./components/App');
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>
