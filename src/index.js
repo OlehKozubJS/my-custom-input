a0ac568f3924941167326d2221ff89f983d48113
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-10 22:50:28 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom/client';
-//import './index.css';
 const { StrictMode } = require('react');
 const { createRoot } = require('react-dom/client');
 const { App } = require('./components/App');
