20986836116e46453f9038df06ded89983ca86f2
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-11 23:41:55 +0200

    Update MyInput.jsx

--- src/components/MyInput.jsx
+++ src/components/MyInput.jsx
@@ -2,17 +2,19 @@ import { inputContainer, inputContent, enterButton } from './css';
 import { useState /*, useCallback*/ } from 'react';
 
 const MyInput = ({ children }) => {
-  сonst[(textString, setTextString)] = useState('');
+  const [textString, setTextString] = useState('');
 
   const handleSubmit = event => {
     event.preventDefault();
-
+    setTextString(event.currentTarget.elements.inputContent.value);
     event.currentTarget.reset();
   };
 
   return (
     <form className={inputContainer}>
-      <input className={inputContent}>{children}</input>
+      <input className={inputContent} name="inputContent">
+        {children}
+      </input>
       <button type="submit" onSubmit={handleSubmit} className={enterButton}>
         E
       </button>
