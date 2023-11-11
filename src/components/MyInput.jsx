4d643f2e047ff82275b6720dc800a42726fab4ff
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-11 23:28:46 +0200

    Update MyInput.jsx

--- src/components/MyInput.jsx
+++ src/components/MyInput.jsx
@@ -1,9 +1,12 @@
 import { inputContainer, inputContent, enterButton } from './css';
+import { useState /*, useCallback*/ } from 'react';
 
 const MyInput = ({ children }) => {
+  сonst[(textString, setTextString)] = useState();
+
   return (
     <div className={inputContainer}>
-      <div className={inputContent}>{children}</div>
+      <input className={inputContent}>{children}</input>
       <button className={enterButton}>E</button>
     </div>
   );
