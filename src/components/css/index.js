987ee6e187bce9625062838fc57688241338fb0a
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-11 15:30:36 +0200

    Update index.js

--- src/components/css/index.js
+++ src/components/css/index.js
@@ -4,4 +4,4 @@ const inputContainer = css.inputContainer;
 const inputContent = css.inputContent;
 const enterButton = css.enterButton;
 
-export { css, inputContainer, inputContent, enterButton };
+export { inputContainer, inputContent, enterButton };
