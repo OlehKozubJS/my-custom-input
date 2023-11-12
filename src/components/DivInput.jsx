50497f09b520f4288cb799fb6db30407c260a6f5
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-12 22:22:23 +0200

    Update DivInput.jsx

--- src/components/DivInput.jsx
+++ src/components/DivInput.jsx
@@ -1,4 +1,9 @@
 import { inputContainer, inputContent, enterButton } from './css';
 import { useState } from 'react';
 
-const DivInput = ({ children, onSubmit }) => {};
+const DivInput = ({ children, onSubmit }) => {
+  const [text, setText] = useState('');
+  const [isActive, setIsActive] = useState(false);
+
+  return <div></div>;
+};
