191afe7256aaad4dc2e99e14dd1617bec50b8a1f
OlehKozubJS <oleg.kozub54@gmail.com>
2023-11-14 23:42:39 +0200

    Update keyDownEffect.js

--- src/decorators/keyDownEffect.js
+++ src/decorators/keyDownEffect.js
@@ -1,18 +1,8 @@
-const keyDownEffect = onKeyDown => {
+const keyDownEffect = (onKeyDown, dependencies) => {
   useEffect(() => {
     const handleKeyDown = event => {
       if (event.type !== 'keydown' || keyExceptions.includes(event.key)) return;
-      if (event.key === 'Backspace') {
-        const shorterText = text.slice(0, text.length - 1);
-        setText(shorterText);
-        return;
-      }
-      if (event.key === 'Shift') {
-        setIsUpperCase(true);
-      }
-      const character = isUpperCase ? event.key.toUpperCase() : event.key;
-      setText(text + character);
-      setIsUpperCase(false);
+      onKeyDown();
     };
 
     window.addEventListener('keydown', handleKeyDown);
@@ -20,13 +10,7 @@ const keyDownEffect = onKeyDown => {
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, [text, setText, isUpperCase]);
-
-  return (
-    <div onClick={toggleIsActive} className={inputContent}>
-      {text || children}
-    </div>
-  );
+  }, dependencies);
 };
 
 export { keyDownEffect };
